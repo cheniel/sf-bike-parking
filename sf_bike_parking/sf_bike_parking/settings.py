a5c61670cd7e88e415329c3d4ca10da0faed4f28
cheniel <danielchen627@gmail.com>
2014-09-29 23:40:22 -0700

    trying to fix staticfiles

--- sf_bike_parking/sf_bike_parking/settings.py
+++ sf_bike_parking/sf_bike_parking/settings.py
@@ -80,9 +80,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(PROJECT_PATH, 'static'),
 )
