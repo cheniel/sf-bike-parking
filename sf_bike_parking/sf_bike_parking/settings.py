5d5899f73bd17f6f40762c2427587b5729b05dad
cheniel <danielchen627@gmail.com>
2014-09-30 00:24:58 -0700

    i'll get this eventualy

--- sf_bike_parking/sf_bike_parking/settings.py
+++ sf_bike_parking/sf_bike_parking/settings.py
@@ -80,10 +80,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-# PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_PATH, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, '../static'),
+)
