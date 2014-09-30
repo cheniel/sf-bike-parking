93d482650d56caa35995852842ef61581062ed3c
cheniel <danielchen627@gmail.com>
2014-09-30 00:21:01 -0700

    hopefully fixing missing static files issue

--- sf_bike_parking/sf_bike_parking/urls.py
+++ sf_bike_parking/sf_bike_parking/urls.py
@@ -7,3 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from <app> import settings
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
